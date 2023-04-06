ded4570c51f94c15ad29724dd4cddfefeea17012
Chief Degen <dev@degenreborn.xyz>
2023-04-06 15:41:17 +0800

    update openMysteryBox

--- test/nft/NFTManager.t.sol
+++ test/nft/NFTManager.t.sol
@@ -14,6 +14,7 @@ contract NFTManagerTest is Test, IDegenNFTDefination, INFTManagerDefination {
     address owner;
     address signer;
     address user = address(11);
+    uint256 mainnetFork;
 
     error CallerNotOwner();
 
@@ -175,6 +176,57 @@ contract NFTManagerTest is Test, IDegenNFTDefination, INFTManagerDefination {
 
         assertEq(r, receiver);
         assertEq(a, (percent * price) / degenNFT.PERCENTAGE_BASE());
+    }
+
+    function testUpdateProperty() public {
+        _forkAndUpgradeTo();
+
+        uint256[] memory tokenIds = new uint256[](5);
+        tokenIds[0] = 341;
+        tokenIds[1] = 348;
+        tokenIds[2] = 914;
+        tokenIds[3] = 927;
+        tokenIds[4] = 1965;
+
+        Property[] memory newProprties = new Property[](5);
+
+        for (uint i = 0; i < tokenIds.length; i++) {
+            Property memory property = degenNFT.getProperty(tokenIds[i]);
+            assertEq(property.nameId, 32);
+            assertEq(property.rarity, 4);
+            assertEq(property.tokenType, 0);
+            property.rarity = uint16(3);
+
+            newProprties[i] = property;
+        }
+
+        // signer
+        vm.startPrank(0xe3b0DF60032E05E0f08559f8F4962368ba47339B);
+        nftManager.openMysteryBox(tokenIds, newProprties);
+
+        for (uint i = 0; i < tokenIds.length; i++) {
+            Property memory property = degenNFT.getProperty(tokenIds[i]);
+            assertEq(property.nameId, 32);
+            assertEq(property.rarity, 3);
+            assertEq(property.tokenType, 0);
+        }
+
+        vm.stopPrank();
+    }
+
+    function _forkAndUpgradeTo() internal {
+        // owner
+        vm.startPrank(0xf775Db913e735dDbB0E03c07665C78Bf2751399f);
+
+        mainnetFork = vm.createSelectFork(vm.envString("MAINNET_RPC"));
+        degenNFT = DegenNFT(0x060e571D900454c868E5D1D6e1a0c566A57224B0);
+        nftManager = NFTManager(0x3955641fE1A2367be6Db951910ac05FFcdE14bDf);
+        DegenNFT newDegenNFT = new DegenNFT();
+        NFTManager newNFTManager = new NFTManager();
+
+        degenNFT.upgradeTo(address(newDegenNFT));
+        nftManager.upgradeTo(address(newNFTManager));
+        vm.stopPrank();
     }
 
     function _initialize() internal {
